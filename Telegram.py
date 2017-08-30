64eb6205d28a91a7f66ebd98cae393175c4c7d3c
Yarodash <yaroslav.lukyanets@gmail.com>
2017-08-30 23:55:41 +0300

    MyAlexandria added some fiches
    MyAlexandria added some fiches

--- Telegram.py
+++ Telegram.py
@@ -3,7 +3,6 @@ import TelegramToken
 
 bot = telebot.TeleBot(TelegramToken.Token)
 
-
 users = []
 count = 0
 
@@ -15,17 +14,43 @@ def handler_text(user):
     message = user.text
     print('Message from', user.from_user.first_name, user.from_user.last_name, message)
 
-    boolean = True
-    for i in range(count):
-        if users[i] == user.chat.id:
-            boolean = False
-    if boolean == True:
-        count += 1
-        users.append(user.chat.id)
-        f = open('Users.txt','w')
-        f.writelines("%s\n" % count)
-        f.writelines("%s\n" % i for i in users)
-        f.close()
-        print('New User:', user.from_user.first_name, user.from_user.last_name, user.chat.id)
+    Vrubaem = True
+    if message == '/stop' or message == 'Нет':
+        Vrubaem = False
+
+    if message == '/start' or message == 'Да':
+        boolean = True
+        for i in range(count):
+            if users[i] == user.chat.id:
+                boolean = False
+        if boolean == True:
+            count += 1
+            users.append(user.chat.id)
+            f = open('Users.txt','w')
+            f.writelines("%s\n" % count)
+            f.writelines("%s\n" % i for i in users)
+            f.close()
+            print('New User:', user.from_user.first_name, user.from_user.last_name, user.chat.id)
+        bot.send_message(user.chat.id,'Сейчас функция отправки новостей включена. Вы можете отключить её в любой момент: /stop')
+
+    if message == '/stop':
+        if user.chat.id in users:
+            count -= 1
+            users.remove(user.chat.id)
+            f = open('Users.txt', 'w')
+            f.writelines("%s\n" % count)
+            f.writelines("%s\n" % i for i in users)
+            f.close()
+            print('User Left:', user.from_user.first_name, user.from_user.last_name, user.chat.id)
+
+    if message == '/help':
+        bot.send_message(user.chat.id,'/start - Начать присылать новости')
+        bot.send_message(user.chat.id,'/stop - Прекратить пересылать новости')
+
+    if user.chat.id not in users and Vrubaem:
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+        user_markup.row('Да','Нет')
+        bot.send_message(user.chat.id,'У вас не включена функция отправки новостей, включить?',reply_markup=user_markup)
+
 
 bot.polling(none_stop=True, interval=3)
--- Telegramsend.py
+++ Telegramsend.py
@@ -17,6 +17,6 @@ while True:
             users.append(int(f.readline()))
         f.close()
         for i in range(count):
-            bot.send_message(users[i], "======New Post======" + chr(10) + "<b>" + New_Post['title'] + "</b>" + chr(10) + chr(10)  + New_Post['text'] + chr(10) + chr(10)  + "<a href = \""+New_Post['url']+"\">Читать полностью...</a>",parse_mode='HTML')
+            bot.send_message(users[i], "<pre>======New Post======</pre>" + chr(10) + "<b>" + New_Post['title'] + "</b>" + chr(10) + chr(10)  + New_Post['text'] + chr(10) + chr(10)  + "<a href = \""+New_Post['url']+"\">Читать полностью...</a>",parse_mode='HTML')
     time.sleep(5)
     Last_Post = New_Post
