6eb202fbc54227946966990185cc17963039f357
Yarodash <yaroslav.lukyanets@gmail.com>
2017-08-30 23:27:29 +0300

    Simplified the code
    Simplified the code

--- Parsing.py
+++ Parsing.py
@@ -1,46 +1,35 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-def parse(url,k):
-    k = k % 15
-
+def parse(url):
     response = urllib.request.urlopen(url)
     soup = BeautifulSoup(response.read(),'html.parser')
     table = soup
-    allpost = []
 
-    for row in table.find_all(class_='post-outer'):
-        post = row.find_all(class_='post hentry')
-        title = post[0].find_all(class_='post-title entry-title')
-        text = post[0].find_all(class_='post-body entry-content')
-        posturl = post[0].find_all(class_='post-title entry-title')
-        image = post[0].find_all('table')
+    row = table.find_all(class_='post-outer')
+    post = row[0].find_all(class_='post hentry')
+    title = post[0].find_all(class_='post-title entry-title')
+    text = post[0].find_all(class_='post-body entry-content')
+    posturl = post[0].find_all(class_='post-title entry-title')
+    image = post[0].find_all('table')
 
-        text = text[0].div.text
+    text = text[0].div.text
 
-        title = title[0].a.text
+    title = title[0].a.text
 
-        title = title.replace(u'\xa0',u' ')
-        text = text.replace(u'\xa0',u' ')
+    title = title.replace(u'\xa0',u' ')
+    text = text.replace(u'\xa0',u' ')
 
-        for i in range(len(text)):
-            if text[i] == chr(10) and i > 0:
-                break
+    for i in range(len(text)):
+        if text[i] == chr(10) and i > 0:
+            break
 
+        allpost = []
         allpost.append({
             'title': title,
             'text': text[1:i],
             'url': posturl[0].a.get('href'),
             'imageurl': image[0].a.get('href')
         })
-    #for i in allpost:
-        #print(i.get('title'))
-    #nextpage = soup.find_all(class_='blog-pager')
-    #nextpage = nextpage[0].find_all('span')
-    #print('----------',index)
 
-    #t = 0
-    #for i in nextpage:
-    #   t += 1
-
-    return(allpost[k])
+    return(allpost[0])
--- Telegramsend.py
+++ Telegramsend.py
@@ -8,10 +8,8 @@ bot = telebot.TeleBot(TelegramToken.Token)
 
 Last_Post = []
 
-k = 0
 while True:
-    New_Post = Parsing.parse('https://myalexandriya.blogspot.com/',k)
-    k += 1
+    New_Post = Parsing.parse('https://myalexandriya.blogspot.com/')
     if Last_Post != New_Post:
         f = open('Users.txt')
         count = int(f.readline())
@@ -19,15 +17,7 @@ while True:
         for i in range(count):
             users.append(int(f.readline()))
         f.close()
-        print(New_Post['title'])
-        print('------------')
         for i in range(count):
-            url = New_Post['imageurl']
-            urllib2.urlretrieve(url, 'MyAlexandriaImage.jpg')
-            #img = open('MyAlexandriaImage.jpg', 'rb')
             bot.send_message(users[i], "======New Post======" + chr(10) + "<b>" + New_Post['title'] + "</b>" + chr(10) + chr(10)  + New_Post['text'] + chr(10) + chr(10)  + "<a href = \""+New_Post['url']+"\">Читать полностью...</a>",parse_mode='HTML')
-            #bot.send_photo(users[i], img)
-            #img.close()
     time.sleep(5)
-
     Last_Post = New_Post
